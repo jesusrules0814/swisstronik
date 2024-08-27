3f2d0227656c68003570f4e80ea9da09110ab688
John Hiđ <stephendinh121@godsgateglobal.com>
2024-08-27 07:44:47 +0700

    first commit

--- scripts/proxy/utils/deployed-address.ts
+++ scripts/proxy/utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xaCF84CED4eedD053C0417082F09bf2bB2d45899E'
-export const ProxyAdmin = '0xC72182487A5D82aD21F26B02900b9B7265442355'
-export const SWTRImplementation = '0xE75eD02BA12089212471dB9CB22AdFa577ea2Af5'
+export const SWTRProxy = '0x9915cbB47e2eccE1A9d4f44B95754bb470a2Beef'
+export const ProxyAdmin = '0x42A936c2BB9D0Ba6d083987DE006761E9b61204E'
+export const SWTRImplementation = '0xA3a5E13e8A40216509C459288fb37F815f3c9688'
     
